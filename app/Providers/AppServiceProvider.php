c2e005aaf8bb2134a2b23f4aaaa2c4bde6d058f4
Naguchi <lucrerakuri@gmail.com>
2018-04-17 23:54:03 +0900

    【共通】DB接続

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,4 +15,21 @@ class AppServiceProvider extends ServiceProvider
     {
         //
     }
+
+    public function boot()
+    {
+        \DB::listen(function ($query) {
+            \Log::info("[SQL] " . $this->gemerateSqlQuery($query->sql, $query->bindings));
+        });
+    }
+
+    protected function gemerateSqlQuery(string $query, array $params) {
+        foreach($params as $replace) {
+            $target_pos = strpos($query, '?');
+            $query = preg_replace("/\?/", "", $query, 1);
+            $query = mb_substr($query, 0, $target_pos) . $replace . mb_substr($query, $target_pos, strlen($query));
+        }
+
+        return $query;
+    }
 }
