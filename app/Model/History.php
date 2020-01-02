32b3a2d1300e81afec55f4279e417b6568ddfd55
Unknown <lucrerakuri@gmail.com>
2020-01-03 00:02:47 +0900

    ログにIPとエージェントを追加

--- app/Model/History.php
+++ app/Model/History.php
@@ -2,6 +2,7 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 use DB;
 
 class History extends Model
@@ -9,4 +10,14 @@ class History extends Model
     protected $guarded = [
         'id'
     ];
+
+    public static function requestLog(Request $request)
+    {
+        $logging = new self;
+        $logging->endpoint = $request->url();
+        $logging->parameter = json_encode($request->query());
+        $logging->user_agent = $request->header('User-Agent');
+        $logging->ip_address = $request->ip();
+        $logging->save();
+    }
 }
